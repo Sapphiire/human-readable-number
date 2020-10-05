646ba630112fb36c4466929063811f00addb3ad1
Vadzim <Sapphiire@github.com>
2020-10-05 22:35:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
 module.exports = function toReadable (number) {
-  if(number == 0) return 'zero';
-  const beforeDecades = [
-      'zero',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine',
-      'ten',
-      'eleven',
-      'twelve',
-      'thirteen',
-      'fourteen',
-      'fithteen',
-      'sixteen',
-      'seventeen',
-      'eighteen',
-      'nineteen'
-  ]
-  const decades = [
-      'ten',
-      'twenty',
-      'thirty',
-      'forty',
-      'fifty',
-      'sixty',
-      'seventy',
-      'eighty',
-      'ninety'
-  ]
+    if(number == 0) return 'zero';
+    const beforeDecades = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fithteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ]
+    const decades = [
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ]
 
-  let result = []
-  const hundreds = Math.floor(number / 100)
-  const decade = number % 100
-  const digit = number % 10
-  if(hundreds) result.push(beforeDecades[hundreds]).push('hundred');
-  
-  if(decade != 0) {
-    if(decade <= 19) result.push(beforeDecades[decade]);
-    else {
-        result.push(decades[Math.floor(decade/10) - 1]);
-        if(digit) result.push(beforeDecades[digit]);
+    let result = []
+    const hundreds = Math.floor(number / 100)
+    const decade = number % 100
+    const digit = number % 10
+    if(hundreds) result.push(beforeDecades[hundreds] + ' hundred');
+
+    if(decade != 0) {
+        if(decade <= 19) result.push(beforeDecades[decade]);
+        else {
+            result.push(decades[Math.floor(decade/10) - 1]);
+            if(digit) result.push(beforeDecades[digit]);
+        }
     }
-  }
-  
-  return result.join(' ')
+
+    return result.join(' ')
 }
