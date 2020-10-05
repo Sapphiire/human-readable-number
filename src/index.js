9a1ddd0ee7f6a08f05bad9a348de1fbcdcc5c84d
Vadzim <Sapphiire@github.com>
2020-10-05 21:49:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function toReadable (number) {
+  if(number == 0) return 'zero';
+  const beforeDecades = [
+      'zero',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'eight',
+      'nine',
+      'ten',
+      'eleven',
+      'twelve',
+      'thirteen',
+      'fourteen',
+      'fithteen',
+      'sixteen',
+      'seventeen',
+      'eighteen',
+      'nineteen'
+  ]
+  const decades = [
+      'ten',
+      'twenty',
+      'thirty',
+      'forty',
+      'fifty',
+      'sixty',
+      'seventy',
+      'eighty',
+      'ninety'
+  ]
+  const hundred = 'hundred'
+
+  let result = []
+  const hundreds = Math.floor(number / 100)
+  const decade = number % 100
+  const digit = number % 10
+  if(hundreds) result.push(beforeDecades[hundreds] + ' ' + hundred);
+  if(decade <= 19) result.push(beforeDecades[decade]);
+  else {
+    result.push(decades[Math.floor(decade/10) - 1])
+    if(digit) result.push(beforeDecades[digit])
+  }
   
+  return result.join(' ')
 }
